b0a58d483f47c2e644e3ccb7e4abbeb24da9b031
Roma Patel <patelroma501@gmail.com>
2021-01-22 14:13:42 -0800

    JS Basics

---
+++ JS/DataTypes/DataTypes.js
@@ -0,0 +1,16 @@
+var emp_Name = "Roma";
+var emp_Sal = 75000;
+var is_Avail = true;
+var loc;
+var mobile = null;
+
+
+// How to display DataType of varible?
+console.log(typeof emp_Name);
+console.log(typeof emp_Sal);
+
+console.log(typeof is_Avail);
+
+console.log(typeof loc);
+
+console.log("Mobile Number - Data Type", typeof mobile);
---
+++ JS/DataTypes/NonPrimitive.js
@@ -0,0 +1,12 @@
+// let size = []; // Empty Array
+
+let emp_Obj = {
+    name: "Roma",
+    email: "romapatel@gmail.com",
+} // Object
+
+console.log(typeof emp_Obj);
+
+let size = [30, 40, 50, 60]; 
+
+console.log(typeof size);
